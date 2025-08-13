c153f22cda20c3e6dc95034d0dd072c4aa90b3c1
CatHood0 <santiagowmar@gmail.com>
2025-08-13 09:45:15 -0400

    Fix: node_test that are not doing what supposed

--- test/nodes/nodes_test.dart
+++ test/nodes/nodes_test.dart
@@ -1,5 +1,6 @@
 import 'package:easy_rich_editor/easy_rich_editor.dart';
-import 'package:flutter_quill_delta_easy_parser/flutter_quill_delta_easy_parser.dart' as pr;
+import 'package:flutter_quill_delta_easy_parser/flutter_quill_delta_easy_parser.dart'
+    as pr;
 import 'package:flutter_test/flutter_test.dart';
 
 import '../resources/doc_rs.dart';
@@ -8,27 +9,21 @@ void main() {
   test('jump to parent effectively', () {
     final pr.Document doc = commonDoc;
     final Node root = DocumentToNodesParser.documentParse(doc);
-    final Node paragraph = root.elementAt(3);
+    final Node paragraph = root.queryPath(<int>[3])!;
     final Node node = Node(
       type: ParagraphKeys.lineKey,
-      value: [
+      value: <pr.TextFragment>[
         pr.TextFragment(data: "This is my example text bitch. "),
         pr.TextFragment(data: "So, i want to know "),
-        pr.TextFragment(data: "why (2)", attributes: {'bold': true}),
+        pr.TextFragment(
+            data: "why (2)", attributes: <String, dynamic>{'bold': true}),
       ],
       id: 'Test id',
       canModifyChildrenLength: false,
     );
 
     paragraph.insertNode(node, path: 0, after: true);
-    final rootNode = node.jumpToParent(stopAt: (node) {
-      // if the parent of the cur parent, is null
-      // means that we are currently at the root
-      if (node.parent?.parent == null) {
-        return true;
-      }
-      return false;
-    });
+    final Node rootNode = node.jumpToParentExceptRoot()!;
 
     expect(rootNode.id, paragraph.id);
     expect(rootNode, paragraph);
@@ -37,18 +32,22 @@ void main() {
   test('just a simple text to know if the tree works as expected', () {
     final pr.Document doc = commonDoc;
     final Node root = DocumentToNodesParser.documentParse(doc);
-    final Node paragraph = root.elementAt(3);
+    final Node paragraph = root.queryPath(<int>[3])!;
     final Node node = Node(
       type: ParagraphKeys.lineKey,
-      value: [
+      value: <pr.TextFragment>[
         pr.TextFragment(data: "This is my example text bitch. "),
         pr.TextFragment(data: "So, i want to know "),
-        pr.TextFragment(data: "why", attributes: {'bold': true}),
+        pr.TextFragment(
+            data: "why", attributes: <String, dynamic>{'bold': true}),
       ],
       id: 'Test id',
       canModifyChildrenLength: false,
     );
 
     paragraph.insertNode(node, path: 0, after: false);
+    expect(node.parent, isNotNull);
+    expect(node.parent, paragraph);
+    expect(paragraph.contains(node.id), isTrue);
   });
 }
