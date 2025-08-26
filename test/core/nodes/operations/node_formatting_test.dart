5b17d47668d465862fbf354efd7fb6373ea98b41
CatHood0 <santiagowmar@gmail.com>
2025-08-26 16:54:40 -0400

    Chore(test): added test cases for formatting

--- test/core/nodes/operations/node_formatting_test.dart
+++ test/core/nodes/operations/node_formatting_test.dart
@@ -1,5 +1,6 @@
 import 'package:easy_attribution_text/easy_text.dart';
 import 'package:easy_rich_editor/easy_rich_editor.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import '../../../resources/doc_rs.dart';
 
@@ -12,13 +13,63 @@ void main() {
 
   test('should format a single char', () {
     expect(root, isNotNull);
-    final Node? line = root!.queryPath(<int>[2, 0]);
-    expect(line, isNotNull);
-    final FragmentChangeContext context = line!.format(
+    final Node line = root!.queryPath(<int>[2, 0])!;
+    expect(line.texts.length, equals(1));
+    expect(
+      line.texts.first.text,
+      'This is just a simple document where we can just '.characters,
+    );
+    line.format(
       2,
       1,
       formatBlock: false,
       attributes: EasyAttributeStyles.fromAttribute(BoldAttribute()),
+    );
+
+    expect(line.texts.length, equals(3));
+    expect(line.texts.first.text, 'Th'.characters);
+    expect(
+      line.texts.first.styles,
+      equals(EasyAttributeStyles.empty()),
+    );
+    EasyText? next = line.texts.first.next;
+    expect(next, isNotNull);
+    expect(next!.text, 'i'.characters);
+    expect(
+      next.styles,
+      equals(EasyAttributeStyles.fromAttribute(BoldAttribute())),
+    );
+    expect(next.next, equals(line.texts.last));
+  });
+
+  test('should format two times selections in the same block line', () {
+    expect(root, isNotNull);
+
+    final Node line = root!.queryPath(<int>[2, 0])!;
+    expect(line.texts.length, equals(1));
+    expect(
+      line.texts.first.text,
+      'This is just a simple document where we can just '.characters,
+    );
+    line.format(
+      2,
+      1,
+      formatBlock: false,
+      attributes: EasyAttributeStyles.fromAttribute(BoldAttribute()),
+    );
+
+    expect(line.texts.length, equals(3));
+    expect(line.texts.first.text, 'Th'.characters);
+    expect(
+      line.texts.first.styles,
+      equals(EasyAttributeStyles.empty()),
+    );
+    EasyText? next = line.texts.first.next;
+    expect(next, isNotNull);
+    expect(next!.text, 'i'.characters);
+    expect(
+      next.styles,
+      equals(EasyAttributeStyles.fromAttribute(BoldAttribute())),
     );
     line.format(
       14,
@@ -26,8 +77,21 @@ void main() {
       formatBlock: false,
       attributes: EasyAttributeStyles.fromAttribute(ItalicAttribute()),
     );
-    print(line.dumpTreeStr());
-    expect(context.executed, isTrue);
+    expect(line.texts.length, equals(5));
+    expect(next.next, isNotNull);
+    next = next.next;
+    expect(next, isNotNull);
+    expect(next!.text, equals('s is just a si'.characters));
+    expect(next.styles, equals(EasyAttributeStyles.empty()));
+    next = next.next;
+    expect(next, isNotNull);
+    // moves to the node where were applied the italic attribution
+    expect(next!.text, 'mple'.characters);
+    expect(
+      next.styles,
+      equals(EasyAttributeStyles.fromAttribute(ItalicAttribute())),
+    );
+    expect(next.next, equals(line.texts.last));
   });
   test('should format entire block children', () {});
   test('should format single block attributes', () {});
